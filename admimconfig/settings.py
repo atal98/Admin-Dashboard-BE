b889d9f48fdbd944f7310a930653d28f68cee7bd
atal98 <80337520+atal98@users.noreply.github.com>
2024-06-03 20:07:59 +0530

    auth is disable for now

--- admimconfig/settings.py
+++ admimconfig/settings.py
@@ -157,29 +157,29 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-        # Add other authentication classes here if needed
-    ),
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ]
-}
+# REST_FRAMEWORK = {
+#     'DEFAULT_AUTHENTICATION_CLASSES': (
+#         'rest_framework_simplejwt.authentication.JWTAuthentication',
+#         # Add other authentication classes here if needed
+#     ),
+#     'DEFAULT_PERMISSION_CLASSES': [
+#         'rest_framework.permissions.IsAuthenticated',
+#     ]
+# }
 
-SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=360),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-    # 'ROTATE_REFRESH_TOKENS': False,
-    # 'BLACKLIST_AFTER_ROTATION': True,
-    # 'ALGORITHM': 'HS256',
-    # 'SIGNING_KEY': SECRET_KEY,
-    # 'VERIFYING_KEY': None,
-    # 'AUTH_HEADER_TYPES': ('Bearer',),
-    # 'USER_ID_FIELD': 'id',
-    # 'USER_ID_CLAIM': 'user_id',
-    # 'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
-    # 'TOKEN_TYPE_CLAIM': 'token_type',
-    # 'JTI_CLAIM': 'jti',
+# SIMPLE_JWT = {
+#     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=360),
+#     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+#     # 'ROTATE_REFRESH_TOKENS': False,
+#     # 'BLACKLIST_AFTER_ROTATION': True,
+#     # 'ALGORITHM': 'HS256',
+#     # 'SIGNING_KEY': SECRET_KEY,
+#     # 'VERIFYING_KEY': None,
+#     # 'AUTH_HEADER_TYPES': ('Bearer',),
+#     # 'USER_ID_FIELD': 'id',
+#     # 'USER_ID_CLAIM': 'user_id',
+#     # 'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+#     # 'TOKEN_TYPE_CLAIM': 'token_type',
+#     # 'JTI_CLAIM': 'jti',
 
-}
+# }
